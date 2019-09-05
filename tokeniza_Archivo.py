2d9ae9157701261863035ce113fd766d93615f92
Ismi <ismarigmp@gmail.com>
2019-09-05 08:49:07 -0500

    De la practica 3, en la seccion 2.1 quedó tokenización (script tokenizar), trabajando en la función

--- tokeniza_Archivo.py
+++ tokeniza_Archivo.py
@@ -5,6 +5,7 @@ toktok = ToktokTokenizer()
 #def tokens():
 # Tokenizador de oraciones
 es_tokenizador_oraciones = nltk.data.load("tokenizers/punkt/spanish.pickle")
+
 def tokens():
     with open('C:/Users/Guiller Is/Documents/3_SEMESTRE/PLN/practica3.txt','r',encoding="utf8")as file:
         parrafo=file.read()
@@ -15,7 +16,7 @@ def tokens():
 
     for s in texto:
 
-     guarda_palabras=guarda_palabras+[t for t in toktok.tokenize(s)]
+        guarda_palabras=guarda_palabras+[t for t in toktok.tokenize(s)]
 
     return guarda_palabras
     print(guarda_palabras)
